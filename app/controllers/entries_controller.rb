f11dde3de2303e3b012c861d1ea0803c733e16c1
philliproyster-1 <phillip.royster@kellogg.northwestern.edu>
2026-02-24 04:42:29 +0000

    Complete Travel Log assignment

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -7,7 +7,7 @@ class EntriesController < ApplicationController
     @entry = Entry.new
     @entry.title = params["title"]
     @entry.description = params["description"]
-    @entry.posted_on = params["posted_on"]
+    @entry.occurred_on = params["occurred_on"]
     @entry.place_id = params["place_id"]
     @entry.save
     
