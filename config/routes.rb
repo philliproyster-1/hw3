f906c4e6ea9ba1f82b78dbdfad100e338673f22e
philliproyster-1 <phillip.royster@kellogg.northwestern.edu>
2026-02-24 03:58:50 +0000

    hw3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Root route
+  get("/", { :controller => "places", :action => "index" })
 
-  # Defines the root path route ("/")
+  # Places Routes
+  get("/places", { :controller => "places", :action => "index" })
+  get("/places/new", { :controller => "places", :action => "new" })
+  post("/places", { :controller => "places", :action => "create" })
+  get("/places/:id", { :controller => "places", :action => "show" })
+
+  # Entries Routes
+  get("/entries/new", { :controller => "entries", :action => "new" })
+  post("/entries", { :controller => "entries", :action => "create" })
+
   # get("/", { :controller => "articles", :action => "index" })
 end
